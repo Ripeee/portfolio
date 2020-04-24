99769b840dc239198ad951b676f221072314b84b
Ripeee <63798178+Ripeee@users.noreply.github.com>
2020-04-24 19:19:05 +0700

    Bagian Utama Website
    Website

---
+++ utama.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Project Team B-14</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="stylesheet.css">
+  </head>
+ <!-- Navbar-->
+
+  <body>
+    
+      <!-- Bagian Utama -->
+    <div class="top-wrapper">
+      <div class="container">
+        <h1>WELCOME TO WEBSITE</h1>
+        <h1>DTS-B14</h1>
+        <p>Website abstrak dari tim B14.</p>
+        <p>Hasil Pergabutan karena Pandemi Covid-19</p>
+       
+      </div>
+    </div>
+      <!-- Akhir Bagian utama -->
+  </body>
+</html>
