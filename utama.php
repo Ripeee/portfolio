d2f64d49b92d460e3b6d05908494a17fa29bc246
Ripeee <63798178+Ripeee@users.noreply.github.com>
2020-04-27 09:30:34 +0700

    ganti header, tambah extra & footer

--- utama.php
+++ utama.php
@@ -17,14 +17,12 @@
     
        <!-- Header -->
   <header>
-    <div class="header-logo">B14</div>
-      <div class="header-list">
-        <ul>
-          <li><a href="#">Profil</a></li>
-          <li><a href="#">Tentang</a></li>
-          <li><a href="#">Testimonial</a></li>
-          <li><a href="#">Hubungi Kami</a></li>
-        </ul>
+    <div class="container">
+      <div class="header-left">B14</div>
+      <span class="menu-icon fa fa-bars "></span>
+      <div class="header-right">
+        <li><a href="#profile">Profile</a></li>
+        <li><a href="#hubungi">Hubungi Kami</a></li>
       </div>
     </div>
   </header>
@@ -43,36 +41,156 @@
       <!-- Akhir Bagian utama -->
   
       <!-- Bagian Extra -->
-  <div class="main">     
-    <div class="contents">
-      <h3 class="section-title">Profile Team DTS_B14</h3>
-      <div class="contents-item">
-        <img src="img/ari.jpg" class="img-responsive">
-        <p>ARI PURNOMO AJI<br>
-        0447677131-104</p>
-      </div>
-      <div class="contents-item">
-        <img src="img/andrian.jpg" class="img-responsive">
-        <p>ANDRIAN JULIANSYAH<br>
-        0447677021-98</p>
-      </div>
-      <div class="contents-item">
-        <img src="img/angga.jpg" class="img-responsive">
-        <p>ANGGA MAHENDRA<br>
-        0447677021-98</p>
-      </div>
-      <div class="contents-item">
-        <img src="img/anggun.jpg" class="img-responsive">
-        <p>ANGGUN MUSTIKASARI DEWI<br>
-        0447677120-43</p>
-      </div>
-      <div class="contents-item">
-        <img src="img/yuni.jpg" class="img-responsive">
-        <p>AYUNI SARI<br>
-        0447677030-25</p>
+    <!--Bagian Extra 1-->
+  <div class="main" id="profile">
+    <h3 class="section-title">PROFILE TEAM B14</h3>        
+    <div class="container">
+      <div class="contents">
+        <div class="content">
+          <div class="contents-item item1">
+            <img src="img/ari.jpg" alt="" class="img-responsive">
+                <p>ARI PURNOMO AJI<br>0447677131-104</p>
+                <div class="social">
+                  <a href="#"><i class="fa fa-facebook"></i></a>
+                  <a href="#"><i class="fa fa-twitter"></i></a>
+                  <a href="#"><i class="fa fa-instagram"></i></a>
+                </div>
+          </div>
+        </div>
+        <div class="content">
+          <div class="contents-item item2">
+            <img src="img/yuni.jpg" alt="" class="img-responsive">
+            <p>AYUNI SARI<br>0447677030-25</p>
+            <div class="social">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="content">
+          <div class="contents-item item3">
+            <img src="img/andrian.jpg" alt="" class="img-responsive">
+            <p>ANDRIAN JULIANSYAH<br>0447677021-98</p>
+            <div class="social">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="content">
+          <div class="contents-item item4">
+            <img src="img/anggun.jpg" alt="" class="img-responsive">
+            <p>ANGGUN MUSTIKASARI DEWI<br>0447677120-43</p>
+            <div class="social">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="content">
+          <div class="contents-item item5">
+            <img src="img/angga.jpg" alt="" class="img-responsive">
+            <p>ANGGA MAHENDRA<br>0447677021-98</p>
+            <div class="social">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-          <!-- Akhir Bagian Extra -->
+
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script>
+  // media query event handler
+
+  function myFunction(x) {
+    if (x.matches) { // If media query matches
+      $('.contents').slick({
+          centerMode: true,
+          slidesToShow: 1
+        });
+      }
+    else {
+      $('.contents').slick({
+          centerMode: true,
+          slidesToShow: 1
+        });
+      }
+  }
+  var x = window.matchMedia("(max-width: 1170px)")
+  myFunction(x)
+  x.addListener(myFunction)
+
+</script>
+
+    <!--Bagian Extra 2-->
+
+  <div class="extra2-bag" id="hubungi">
+    <h3 class="section-title">Hubungi Kami</h3>
+    <div class="container">
+      <div class="extra2">
+        <div class="extra2-item">
+          <img src="img/learn.jpg" alt="">
+            <div class="extra2-text">
+              <h5>Learn</h5>
+              <p>Kami belajar HTML&CSS untuk membuat Web</p>
+            </div>
+        </div>
+        <div class="extra2-item">
+          <img src="img/team.png" alt="">
+            <div class="extra2-text">
+              <h5>Team</h5>
+              <p>Kami bekerja dan belajar bersama</p>
+            </div>
+        </div>
+        <div class="extra2-item">
+          <img src="img/webdesign.jpg" alt="">
+            <div class="extra2-text">
+              <h5>Web Design</h5>
+              <p>Kami membuat web design</p>
+            </div>
+        </div>
+        <div class="extra2-item">
+          <img src="img/js.png" alt="">
+            <div class="extra2-text">
+              <h5>Future</h5>
+              <p>Kami akan belajar JavaScript</p>
+            </div>
+        </div>
+      </div>
+      <div class="contact-form">
+        <div class="contact">
+          <p>Email<span>*</span></p>
+          <input>
+          <p>Pesan<span>*</span></p>
+          <textarea></textarea>
+          <p><span>*</span>wajib diisi</p>
+          <input class="btn message" type="submit" value="Kirim">
+        </div>
+      </div>
+    </div>
+  </div>
+    <!-- Akhir Bagian Extra -->
+    <!--Footer-->
+  <footer>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-sm-12">
+          <p>&copy; copyright 2020 | Built by DTS-B14 </p>
+        </div>
+      </div>
+    </div>
+  </footer>
   </body>
 </html>
